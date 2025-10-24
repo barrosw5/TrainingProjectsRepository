a8c8c48a02519c40637d9d052360967ee35f0c20
Martim Barros <mpdb2016@gmail.com>
2025-10-24 13:13:24 +0100

    Add SoftwareEngineer API and update Java version
    Introduced SoftwareEngineer model and SoftwareEngineerController to provide a REST endpoint for listing engineers. Updated Application class to include a basic hello world endpoint. Changed Java version in pom.xml from 25 to 24.

---
+++ spring-boot/src/main/java/com/training/spring_boot/SoftwareEngineer.java
@@ -0,0 +1,73 @@
+package com.training.spring_boot;
+
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer(){
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techStack) {
+        this.id = id;
+        this.name = name;
+        this.techStack = techStack;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.techStack);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SoftwareEngineer other = (SoftwareEngineer) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.techStack, other.techStack)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+}
